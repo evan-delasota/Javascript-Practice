be5d5f82db553746057174edae185ee956189da5
Evan D <Evan_delasota@yahoo.com>
2021-01-29 07:22:30 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const computer = {
+    name: "Personal Computer",
+
+    motherboard: {
+        name: "Aorus Gaming 7",
+        cpu: "Core i5-8600k",
+        ram: "Corsair Vengeance 16gb",
+        cooler: "Noctua DH15",
+        os: "Windows 10"
+    },
+
+    gpu: "EVGA 3080 FTW Ultra",
+    psu: "EVGA 750w",
+    
+    peripherals : {
+        mouse: "Logitech G502",
+        keyboard: "Durgod Tenkeyless Mechanical Keyboard"
+    }
+};
+
+console.log("Computer: ", computer.name);
+console.log("Motherboard: ", computer.motherboard.name);
+console.log("CPU: ", computer.motherboard.cpu);
+console.log("GPU: ", computer.gpu);
